abb3e478a92edf3b087d8dbb570887f5a8386d60
mjelenabg <mjelenabg@gmail.com>
2015-04-01 15:47:42 +0100

    Napravljena klasa Menjacnica koja implementira MenjacnicaInterfejs.

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(Valuta v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void obrisiKurs(Valuta v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Valuta nadjiKurs(String nazivValute, String skraceniNazivValute,
+			GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
